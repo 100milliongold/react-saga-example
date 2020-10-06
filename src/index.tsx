d581a795a03ddfddd65b45ffd86cc8f903a5ddce
je.kim <gadian88@gmail.com>
2020-10-06 21:52:24 +0900

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './modules';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+// import Thunk from "redux-thunk";
+// import rootReducer from "./modules";
 
-const store = createStore(rootReducer);
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
+
+// const store = createStore(rootReducer, applyMiddleware(Thunk));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/modules/github/reducer.ts
@@ -0,0 +1,73 @@
+import { createReducer } from "typesafe-actions";
+import { GithubState, GithubAction } from "./types";
+import {
+  GET_USER_PROFILE,
+  GET_USER_PROFILE_SUCCESS,
+  GET_USER_PROFILE_ERROR,
+} from "./actions";
+import { getUserProfileAsync } from "./actions";
+
+import { asyncState, createAsyncReducer } from "../../lib/reducerUtils";
+
+const initialState: GithubState = {
+  userProfile: {
+    loading: false,
+    error: null,
+    data: null,
+  },
+};
+
+// const github = createReducer<GithubState, GithubAction>(initialState, {
+//   [GET_USER_PROFILE]: (state) => ({
+//     ...state,
+//     userProfile: {
+//       loading: true,
+//       error: null,
+//       data: null,
+//     },
+//   }),
+//   [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
+//     ...state,
+//     userProfile: {
+//       loading: false,
+//       error: null,
+//       data: action.payload,
+//     },
+//   }),
+//   [GET_USER_PROFILE_ERROR]: (state, action) => ({
+//     ...state,
+//     userProfile: {
+//       loading: false,
+//       error: action.payload,
+//       data: null,
+//     },
+//   }),
+// });
+
+// const github = createReducer<GithubState, GithubAction>(initialState, {
+//   [GET_USER_PROFILE]: (state) => ({
+//     ...state,
+//     userProfile: asyncState.load(),
+//   }),
+//   [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
+//     ...state,
+//     userProfile: asyncState.success(action.payload),
+//   }),
+//   [GET_USER_PROFILE_ERROR]: (state, action) => ({
+//     ...state,
+//     userProfile: asyncState.error(action.payload),
+//   }),
+// });
+
+const github = createReducer<GithubState, GithubAction>(
+  initialState
+).handleAction(
+  [
+    getUserProfileAsync.request,
+    getUserProfileAsync.success,
+    getUserProfileAsync.failure,
+  ],
+  createAsyncReducer(getUserProfileAsync, "userProfile")
+);
+
+export default github;
